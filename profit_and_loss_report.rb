1a814a5d622fc4981a9515048bb0a0550d17f55b
John Mortlock <john.mortlock@gmail.com>
2016-08-27 11:41:50 +0930

    Parse the header and normalise the dates into a standard range, should make comparison easier.

--- profit_and_loss_report.rb
+++ profit_and_loss_report.rb
@@ -1,21 +1,32 @@
+require 'date'
+
 class ProfitAndLossReport
+  attr_accessor :first_month
 
-def initialize
-  @accounts = Array.new
-end
+  def initialize
+    @accounts = Array.new
+    @first_month = 0;
+  end
 
-def add_account(account)
-  @accounts << account
-end
+  private
+  def get_month_name(index)
+    return Date::MONTHNAMES[index]
+  end
 
-def render
-  @accounts.each do |account|
-    puts "Account: #{account.id} and #{account.description}"
-    account.income.each do |key,value|
-      puts "Month #{key} with value #{value}"
+  public
+  def add_account(account)
+    @accounts << account
+  end
+
+  def render
+    @accounts.each do |account|
+      puts "Account: #{account.id} and #{account.description}"
+      account.income.each do |key,value|
+        month_name = get_month_name(key)
+        puts "Month #{month_name} with value #{value}"
+      end
+      puts "Total is #{account.total}"
     end
-    puts "Total is #{account.total}"
- end
-end
+  end
 
 end
--- report_csv_importer.rb
+++ report_csv_importer.rb
@@ -4,30 +4,73 @@ require_relative 'account'
 require_relative 'profit_and_loss_report'
 
 class ReportCSVImporter
+
+  def initialize
+    @first_month = 0;
+    @header_parsed = false;
+  end
+
   private
+
+  # We want to map the columns in the report to a normal
+  # month index range (0-11)
+  def normalise_month(row_index)
+    month_index = row_index + @first_month
+    if month_index > 12
+      month_index -= 12
+    end
+    return month_index
+  end
+
+  # Convert the row data into an account record.
   def convert_row_to_account(row)
     account = Account.new();
     account.id = row[0].strip
     account.description = row[1].strip
     for i in 2..row.size-2
-      account.set_month_income(i-2, row[i])
+      account.set_month_income(normalise_month(i-2), row[i])
     end
     return account
   end
 
+  # Parse the header row,basically just need the first month.
+  # first month lives in column 2.
+  def parse_header(row)
+    @first_month = 0 #January.
+    row.each do |col|
+      if !col.nil?
+        col_name = col.strip
+        index = Date::MONTHNAMES.index(col_name)
+        if !index.nil?
+          @first_month = index
+          break
+        end
+      end
+    end
+    @header_parsed = true
+  end
+
+  def is_row_valid?(row)
+    return row.size > 7 && !row[0].nil?
+  end
+
+  def parse_row(report, row)
+    if is_row_valid?(row)
+      if !@header_parsed
+        parse_header(row)
+      else
+        report.add_account(convert_row_to_account(row))
+      end
+    end
+  end
+
   public
   def import(file_name)
-    header_parsed = false;
+    @header_parsed = false
+    @first_month = 0
     report = ProfitAndLossReport.new
     CSV.foreach(file_name, converters: :numeric) do |row|
-      if row.size > 7 && !row[0].nil?
-        if header_parsed
-          report.add_account(convert_row_to_account(row))
-        end
-      else
-        puts "Parsing the header fucker."
-        header_parsed = true
-      end
+      parse_row(report, row)
     end
     return report
   end
